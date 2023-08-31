f44dc149e9423e52871f9b8dcd13e2fda379e27a
rootnure <nur.diu.2791@gmail.com>
2023-09-01 03:12:35 +0600

    sorting functionality added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -1,4 +1,5 @@
 let categoryIdNow = '1000';
+let isSortByView = false;
 const loadAllCategories = async () => {
     const apiUrl = `https://openapi.programming-hero.com/api/videos/categories`;
     const res = await fetch(apiUrl);
@@ -11,9 +12,10 @@ const displayAllCategories = categories => {
     const categoriesContainer = document.getElementById('categories-container');
     categories.forEach(category => {
         const categoryBtn = document.createElement('button');
-        categoryBtn.classList = `btn bg-gray-300 font-bold hover:text-black normal-case`;
+        categoryBtn.classList = `btn font-bold hover:text-black normal-case bg-gray-300`;
         if(categories.indexOf(category) === 0) {
             categoryBtn.classList.add('bg-red-500', 'text-white');
+            categoryBtn.classList.remove('bg-gray-300');
         }
         categoryBtn.setAttribute('onclick', `handleActive(this, ${category.category_id})`);
         categoryBtn.innerText = `${category.category}`;
@@ -33,13 +35,18 @@ const loadCategory = async () => {
 
 const displayCategory = videos => {
     const videosContainer = document.getElementById('videos-container');
+    // clearing the previous results from website
     videosContainer.textContent = '';
+    // checking for sorting
+    if(isSortByView) {
+        videos.sort((v1, v2) => (parseFloat(v1.others.views) < parseFloat(v2.others.views)) ? 1 : ((parseFloat(v2.others.views) < parseFloat(v1.others.views)) ? -1 : 0));
+    }
     videos.forEach(video => {
         const { thumbnail, title, authors, others } = video;
         const [author] = authors;
         const videoDiv = document.createElement('div');
         videoDiv.innerHTML = `
-        <div class="card bg-base-100 rounded">
+        <div class="card hover:cursor-pointer hover:scale-105 duration-100 bg-base-100 rounded">
             <figure class="h-52 md:h-40"><img src="${thumbnail}" alt="${title}" class="w-full"></figure>
             <div class="card-body px-1 py-4 grid grid-cols-12 gap-x-4">
                 <div class="col-span-2">
@@ -64,6 +71,7 @@ const handleActive = (target, categoryId) => {
         if(neighbor?.classList?.value) {
             if(neighbor.classList.value.includes('bg-red-500')) {
                 neighbor.classList.remove('bg-red-500', 'text-white');
+                neighbor.classList.add('bg-gray-300');
             }
         }
         else {
@@ -71,7 +79,14 @@ const handleActive = (target, categoryId) => {
         }
     });
     activeElement.classList.add('bg-red-500', 'text-white');
+    activeElement.classList.remove('bg-gray-300');
     categoryIdNow = categoryId;
+    isSortByView = false;
+    loadCategory();
+}
+
+const handleSortByView = () => {
+    isSortByView = true;
     loadCategory();
 }
 
