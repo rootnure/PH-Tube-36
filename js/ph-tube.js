19b8ace8d52a49f5724f6f3794d4e28db6a12658
rootnure <nur.diu.2791@gmail.com>
2023-09-01 02:39:52 +0600

    Dynamic data load on category added with active category btn active

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -12,13 +12,15 @@ const displayAllCategories = categories => {
     categories.forEach(category => {
         const categoryBtn = document.createElement('button');
         categoryBtn.classList = `btn bg-gray-300 font-bold normal-case`;
+        categoryBtn.setAttribute('onclick', `handleActive(this, ${category.category_id})`);
         categoryBtn.innerText = `${category.category}`;
         categoriesContainer.appendChild(categoryBtn);
     });
     loadCategory(categoryIdNow);
 }
 
-const loadCategory = async categoryId => {
+const loadCategory = async () => {
+    const categoryId = categoryIdNow;
     const apiUrl = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`;
     const res = await fetch(apiUrl);
     const data = await res.json();
@@ -28,16 +30,17 @@ const loadCategory = async categoryId => {
 
 const displayCategory = videos => {
     const videosContainer = document.getElementById('videos-container');
+    videosContainer.textContent = '';
     videos.forEach(video => {
         const { thumbnail, title, authors, others } = video;
         const [author] = authors;
         const videoDiv = document.createElement('div');
         videoDiv.innerHTML = `
         <div class="card bg-base-100 rounded">
-            <figure class="h-40"><img src="${thumbnail}" alt="${title}" class="w-full"></figure>
+            <figure class="h-52 md:h-40"><img src="${thumbnail}" alt="${title}" class="w-full"></figure>
             <div class="card-body px-1 py-4 grid grid-cols-12 gap-x-4">
                 <div class="col-span-2">
-                    <img src="${author.profile_picture}" alt="${author.profile_name}" class="w-full rounded-full h-9">
+                    <img src="${author.profile_picture}" alt="${author.profile_name}" class="w-12 h-12 rounded-[100%]">
                 </div>
                 <div class="col-span-10 flex flex-col gap-y-1">
                     <h3 class="text-base font-bold">${title}</h3>
@@ -49,6 +52,24 @@ const displayCategory = videos => {
         `;
         videosContainer.appendChild(videoDiv);
     });
+}
+
+const handleActive = (target, categoryId) => {
+    const activeElement = target;
+    const neighbors = target.parentNode.childNodes;
+    neighbors.forEach(neighbor => {
+        if(neighbor?.classList?.value) {
+            if(neighbor.classList.value.includes('bg-red-500')) {
+                neighbor.classList.remove('bg-red-500');
+            }
+        }
+        else {
+            return;
+        }
+    });
+    activeElement.classList.add('bg-red-500');
+    categoryIdNow = categoryId;
+    loadCategory();
 }
 
 loadAllCategories();
