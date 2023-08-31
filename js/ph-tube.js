2955cb900026aa0aa181fc2943dff2bfce2c9c17
rootnure <nur.diu.2791@gmail.com>
2023-08-31 22:05:12 +0600

    All categiries api test success

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -1,1 +1,8 @@
-console.log('ph-tube.js pichi');
+const loadAllCategories = async () => {
+    const apiUrl = `https://openapi.programming-hero.com/api/videos/categories`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    console.log(data);
+}
+
+loadAllCategories();
