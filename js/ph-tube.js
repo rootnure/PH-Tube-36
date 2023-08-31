8e1a4c5b90e5792cac7027f2316b4ee444b2f57d
rootnure <nur.diu.2791@gmail.com>
2023-08-31 23:34:16 +0600

    Dynamic card added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -23,7 +23,32 @@ const loadCategory = async categoryId => {
     const res = await fetch(apiUrl);
     const data = await res.json();
     const videos = data.data;
-    console.log(videos);
+    displayCategory(videos);
+}
+
+const displayCategory = videos => {
+    const videosContainer = document.getElementById('videos-container');
+    videos.forEach(video => {
+        const { thumbnail, title, authors, others } = video;
+        const [author] = authors;
+        const videoDiv = document.createElement('div');
+        videoDiv.innerHTML = `
+        <div class="card bg-base-100 rounded">
+            <figure class="h-40"><img src="${thumbnail}" alt="${title}" class="w-full"></figure>
+            <div class="card-body px-1 py-4 grid grid-cols-12 gap-x-4">
+                <div class="col-span-2">
+                    <img src="${author.profile_picture}" alt="${author.profile_name}" class="w-full rounded-full h-9">
+                </div>
+                <div class="col-span-10 flex flex-col gap-y-1">
+                    <h3 class="text-base font-bold">${title}</h3>
+                    <p class="text-sm text-gray-400">${author.profile_name}${author.verified ? ` <i class="fa-solid fa-square-check text-blue-500"></i>` : ''}</p>
+                    <p class="text-sm text-gray-400">${others.views} views</p>
+                </div>
+            </div>
+        </div>
+        `;
+        videosContainer.appendChild(videoDiv);
+    });
 }
 
 loadAllCategories();
