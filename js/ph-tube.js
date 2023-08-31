e48880d68c7a974de388811543c1d2ffa4e044ef
rootnure <nur.diu.2791@gmail.com>
2023-08-31 22:17:15 +0600

    Header part hard coded html added

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -2,7 +2,12 @@ const loadAllCategories = async () => {
     const apiUrl = `https://openapi.programming-hero.com/api/videos/categories`;
     const res = await fetch(apiUrl);
     const data = await res.json();
-    console.log(data);
+    const categories = data.data;
+    displayAllCategories(categories);
+}
+
+const displayAllCategories = categories => {
+    console.log(categories);
 }
 
 loadAllCategories();
