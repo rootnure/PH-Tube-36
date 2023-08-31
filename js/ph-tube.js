e6f1a070c1aa9ba03e8a30e33e03088d8492374a
rootnure <nur.diu.2791@gmail.com>
2023-08-31 22:48:24 +0600

    Single categiry api test success

--- js/ph-tube.js
+++ js/ph-tube.js
@@ -1,3 +1,4 @@
+let categoryIdNow = '1000';
 const loadAllCategories = async () => {
     const apiUrl = `https://openapi.programming-hero.com/api/videos/categories`;
     const res = await fetch(apiUrl);
@@ -14,6 +15,15 @@ const displayAllCategories = categories => {
         categoryBtn.innerText = `${category.category}`;
         categoriesContainer.appendChild(categoryBtn);
     });
+    loadCategory(categoryIdNow);
+}
+
+const loadCategory = async categoryId => {
+    const apiUrl = `https://openapi.programming-hero.com/api/videos/category/${categoryId}`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+    const videos = data.data;
+    console.log(videos);
 }
 
 loadAllCategories();
